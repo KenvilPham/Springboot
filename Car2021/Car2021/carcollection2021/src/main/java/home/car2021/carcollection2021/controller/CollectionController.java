d6668459200a143c69074a0887ccb98628f0fec9
KenvilPham <kenvil.pham92@gmail.com>
2021-04-12 19:28:39 +0700

    Car2021 update

--- Car2021/Car2021/carcollection2021/src/main/java/home/car2021/carcollection2021/controller/CollectionController.java
+++ Car2021/Car2021/carcollection2021/src/main/java/home/car2021/carcollection2021/controller/CollectionController.java
@@ -26,7 +26,14 @@ public class CollectionController {
     public String getCollectionCar(Model model) {
         Car[] carList = {
             new Car(1, "Triton 4x4 Premium", "Mitsubishi", 869, "/images/triton.jpg"),
-            new Car(2, "Ranger", "Ford", 1205, "/images/fordranger.jpg")
+            new Car(2, "Ranger", "Ford", 1205, "/images/fordranger.jpg"),
+            new Car(3, "Camry", "Toyota", 1305, "/images/camry.jpg"),
+            new Car(4, "Fadil", "Vinfast", 369, "/images/fadil.jpg"),
+            new Car(5, "Morning", "Kia", 385, "/images/kia_morning.jpg"),
+            new Car(6, "CR-V", "Honda", 1215, "/images/hondacrv.jpg"),
+            new Car(7, "Elantra", "Huyndai", 869, "/images/elantra.jpg"),
+            new Car(8, "Santafe", "Huyndai", 1385, "/images/santafe.jpg"),
+            new Car(9, "Vios", "Toyota", 615, "/images/vios.jpg")
         };
         model.addAttribute(CARS, carList);
         model.addAttribute(APP_NAME, appName);
